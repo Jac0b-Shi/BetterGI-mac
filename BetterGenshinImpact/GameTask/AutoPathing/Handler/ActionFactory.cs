2d4fd750a53f1bcc467187aa80559263bfb5c777
Jac0b_Shi <jac0b@jac0bshi.cn>
2026-07-23 20:38:01 +0800

    fix(pathing): restore Windows collection namespace

--- BetterGenshinImpact/GameTask/AutoPathing/Handler/ActionFactory.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/Handler/ActionFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using BetterGenshinImpact.GameTask.AutoGeniusInvokation.Model;
 using BetterGenshinImpact.GameTask.Common.Job;
